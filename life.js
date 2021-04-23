4be427cd4641141e0149965e945050560d9e28c9
avishek202 <75390559+avishek202@users.noreply.github.com>
2021-04-24 03:23:18 +0600

    basic gui added

---
+++ life.js
@@ -0,0 +1,8 @@
+console.log("D");
+let d = document.getElementById('omg');
+    
+    // d.value = "khela";
+    // console.log('d');
+    d.children[0].innerHTML ="D"
+console.log(d.children[0].value);
+
